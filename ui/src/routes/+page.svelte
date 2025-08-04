549d0cc48b8567dd408ea2457ce14479813d7520
Jake Lawrence <43353705+jake0lawrence@users.noreply.github.com>
2025-08-04 02:00:03 +0000

    fix: update tauri.conf.json and implement clipboard logic

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<h1 class="text-4xl font-bold text-purple-500">
+  Tailwind is alive 🎉
+</h1>
